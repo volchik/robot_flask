ae23e716a07762314e83db1010898c43c2fb0df5
Volkov Artem <volchik84@gmail.com>
2020-04-25 18:16:01 +0300

    Переход на OpenCV 3

--- app/camera.py
+++ app/camera.py
@@ -2,14 +2,15 @@
 # coding: utf-8
 import os
 import time
-from cv2 import cv
+import cv2
 import logging
 
 logger = logging.getLogger(__name__)
 
 class Camera(object):
     def __init__(self, cam_num, cam_mode=1, cam_fps=-1, cam_quality=70, cam_put_date=False):
-        self.capture = cv.CaptureFromCAM(cam_num)
+        #self.capture = cv2.CaptureFromCAM(cam_num)
+        self.capture = cv2.VideoCapture(cam_num)
         self.resolutions = {0: (352,288), 1: (640,480), 2: (1280,720), 3: (1280,1024)}
         self.num     = cam_num
         self.mode    = cam_mode
@@ -28,7 +29,8 @@ class Camera(object):
 
     @width.setter
     def width(self, value):
-        cv.SetCaptureProperty(self.capture, cv.CV_CAP_PROP_FRAME_WIDTH, value)
+        #cv2.SetCaptureProperty(self.capture, cv2.CV_CAP_PROP_FRAME_WIDTH, value)
+        self.capture.set(cv2.CAP_PROP_FRAME_WIDTH, value)
         self._width = value
 
     @property
@@ -37,7 +39,8 @@ class Camera(object):
 
     @height.setter
     def height(self, value):
-        cv.SetCaptureProperty(self.capture, cv.CV_CAP_PROP_FRAME_HEIGHT, value)
+        #cv2.SetCaptureProperty(self.capture, cv2.CV_CAP_PROP_FRAME_HEIGHT, value)
+        self.capture.set(cv2.CAP_PROP_FRAME_HEIGHT, value)
         self._height = value
 
     @property
@@ -46,17 +49,19 @@ class Camera(object):
 
     @fps.setter
     def fps(self, value):
-        cv.SetCaptureProperty(self.capture, cv.CV_CAP_PROP_FPS, value)
+        #cv2.SetCaptureProperty(self.capture, cv2.CV_CAP_PROP_FPS, value)
+        self.capture.set(cv2.CAP_PROP_FPS, value)
         self._fps = value
 
-    @property 
+    @property
     def mode(self):
         return self._mode
 
     @mode.setter
     def mode(self, value):
         self._mode = value
-        self.font  = cv.InitFont(cv.CV_FONT_HERSHEY_SIMPLEX, 0.5*(self.mode+1), 0.5*(self.mode+1), 0, self.mode+1, 8)
+        #self.font  = cv2.InitFont(cv2.CV_FONT_HERSHEY_SIMPLEX, 0.5*(self.mode+1), 0.5*(self.mode+1), 0, self.mode+1, 8)
+        self.font  = cv2.FONT_HERSHEY_SIMPLEX
         resolution = self.resolutions.get(value)
         if resolution != None:
             self.width  = resolution[0]
@@ -75,26 +80,32 @@ class Camera(object):
 
     def get_image(self, put_date=False, frameSpeed=0, frameFps=0):
         red, green, blue = self.textColor
-        textColor = cv.RGB(red, green, blue)
-        img = cv.QueryFrame(self.capture)
+        #textColor = cv2.RGB(red, green, blue)
+        textColor = self.textColor
+        #img = cv2.QueryFrame(self.capture)
+        ret, img = self.capture.read()
 
         #Задержка для нормализации периодического процесса захвата (см. документацию)
-        #По непонятной причине из-за cv.WaitKey вылетает сервер 
+        #По непонятной причине из-за cv2.WaitKey вылетает сервер
         #с ошибкой "Ошибка сегментирования (сделан дамп памяти)" после смены размера картинки
-        #cv.WaitKey(10)
+        #cv2.WaitKey(10)
 
         if put_date:
             text = time.strftime('%d/%m/%Y %H:%M:%S',time.localtime())
-            textSize, baseline = cv.GetTextSize(text,self.font)
-            cv.PutText(img, text, (textSize[1],2*textSize[1]),self.font, textColor)
+            #textSize, baseline = cv2.GetTextSize(text, self.font)
+            textSize, baseline = cv2.getTextSize(text, self.font, 0.5*(self.mode+1), self.mode+1)
+            #cv2.PutText(img, text, (textSize[1],2*textSize[1]), self.font, textColor)
+            cv2.putText(img, text, (textSize[1],2*textSize[1]), self.font, 0.5*(self.mode+1), textColor, self.mode+1)
 
         if frameSpeed:
             text = str(frameSpeed) + 'kb/s  ' + str(frameFps) + 'fps'
-            textSize, baseline = cv.GetTextSize(text,self.font)
-            cv.PutText(img, text, (self.width - textSize[0] - textSize[1], self.height - textSize[1]), 
-                       self.font, textColor)
+            #textSize, baseline = cv2.GetTextSize(text, self.font)
+            textSize, baseline = cv2.getTextSize(text, self.font, 0.5*(self.mode+1), self.mode+1)
+            #cv2.PutText(img, text, (self.width - textSize[0] - textSize[1], self.height - textSize[1]), self.font, textColor)
+            cv2.putText(img, text, (self.width - textSize[0] - textSize[1], self.height - textSize[1]), self.font, 0.5*(self.mode+1), textColor, self.mode+1)
 
-        cv2mat = cv.EncodeImage('.jpeg', img, (cv.CV_IMWRITE_JPEG_QUALITY, self.quality))
+        #cv2mat = cv2.EncodeImage('.jpeg', img, (cv2.CV_IMWRITE_JPEG_QUALITY, self.quality))
+        ret, cv2mat = cv2.imencode('.jpeg', img, (cv2.IMWRITE_JPEG_QUALITY, self.quality))
         return cv2mat.tostring()
 
     def dummy_image(self, filename):
